794af9c1ba9ea43328ee5b6d1854a08a9ae28069
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-10 18:47:43 +0500

    After linters

--- middlewares/db_middleware.py
+++ middlewares/db_middleware.py
@@ -1,10 +1,13 @@
 """
 Модуль, отвечающий за мидлварь, которая взаимодействует с базой данных
 """
-from typing import Callable, Dict, Any, Awaitable
+
+from typing import Any, Awaitable, Callable, Dict
+
 from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject
 from asyncpg.pool import Pool
+
 from database import DataBaseClass
 
 
@@ -14,11 +17,11 @@ class DataBaseMiddleware(BaseMiddleware):
         self.pool = pool
 
     async def __call__(
-            self,
-            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-            event: TelegramObject,
-            data: Dict[str, Any]
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any],
     ) -> Any:
         async with self.pool.acquire() as connect:
-            data['database'] = DataBaseClass(connect)
+            data["database"] = DataBaseClass(connect)
             return await handler(event, data)
