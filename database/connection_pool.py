8196a16b49d25dfff2071a6b4104c79df03e8da5
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-10 18:46:44 +0500

    After linters

--- database/connection_pool.py
+++ database/connection_pool.py
@@ -1,19 +1,25 @@
 """
 Модуль, для создания и хранения пула подключения в классе
 """
+
+from typing import Any, List, Tuple
+
 from asyncpg.pool import Pool
-from typing import List, Tuple, Any
 
 
 class DataBaseClass:
     def __init__(self, pool: Pool):
         self.pool = pool
 
-    async def execute(self, command: str, *args,
-                      fetch: bool = False,
-                      fetchval: bool = False,
-                      fetchrow: bool = False,
-                      execute: bool = False):
+    async def execute(
+        self,
+        command: str,
+        *args,
+        fetch: bool = False,
+        fetchval: bool = False,
+        fetchrow: bool = False,
+        execute: bool = False
+    ):
         if fetch:
             return await self.pool.fetch(command, *args)
         elif fetchval:
@@ -23,6 +29,5 @@ class DataBaseClass:
         elif execute:
             return await self.pool.execute(command, *args)
 
-    async def executemany(self, command: str,
-                          input_data: List[Tuple[Any]]):
+    async def executemany(self, command: str, input_data: List[Tuple[Any]]):
         return await self.pool.executemany(command, input_data)
