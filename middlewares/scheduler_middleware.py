b0e89709593eb7d63c23e4e68c2ca31eac32e921
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-10 18:53:10 +0500

    After linters

--- middlewares/scheduler_middleware.py
+++ middlewares/scheduler_middleware.py
@@ -2,7 +2,9 @@
 Модуль, отвечающий за мидлварь, которая передает в хэндлеры объект cheduler
 для планирования выполнения заданий
 """
-from typing import Callable, Dict, Any, Awaitable
+
+from typing import Any, Awaitable, Callable, Dict
+
 from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -13,10 +15,10 @@ class SchedulerMiddleware(BaseMiddleware):
         self.scheduler = scheduler
 
     async def __call__(
-            self,
-            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-            event: TelegramObject,
-            data: Dict[str, Any]
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any],
     ) -> Any:
-        data['scheduler'] = self.scheduler
+        data["scheduler"] = self.scheduler
         return await handler(event, data)
