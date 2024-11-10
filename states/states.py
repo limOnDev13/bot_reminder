b3bbf61c2bf56eebcca971633dcb3e2db42dd421
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-11-10 18:54:19 +0500

    After linters

--- states/states.py
+++ states/states.py
@@ -1,7 +1,8 @@
 """
 Модуль с классами, отражающими возможные состояния пользователя.
 """
-from aiogram.filters.state import StatesGroup, State
+
+from aiogram.filters.state import State, StatesGroup
 
 
 class FSMReminderCreating(StatesGroup):
